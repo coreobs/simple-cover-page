0cd8f7af85ea3b2a91745a12cc2c18b39b4d4c46
[connor-oreilly] <[oreillyc1999@gmail.com}>
2019-05-26 18:07:16 +1000

    Uploaded website.

---
+++ assets/scripts/all.js
@@ -0,0 +1,27 @@
+// VOLUME CONTORL - This should be good for pretty much everybody, however I wouldn't make it much louder.
+document.getElementById("background").volume = 0.18;
+
+var video = document.getElementById("background");
+var btn = document.getElementById("playpause");
+
+function myFunction() {
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
+        }
+    }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
